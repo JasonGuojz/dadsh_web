d6cff8a02a8df099bdaad940c16f67cb8bf070ca
JasonGuojz <3170102415@zju.edu.cn>
2020-09-24 20:59:13 +0800

    fix bugs: transform = transforms.Compose(

--- src/predictor_app.py
+++ src/predictor_app.py
@@ -44,8 +44,10 @@ with open('./code/imgrecord.txt', 'r') as f:
 def binary_output(query, bit_num=48):
 	model.eval()
 	transform = transforms.Compose(
-		[transforms.Resize(227), transforms.ToTensor(),
-		 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+		[transforms.Resize(256),
+		 transforms.CenterCrop(224),
+		 transforms.ToTensor(),
+		 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 	input = transform(query)
 	with torch.no_grad():
 		input = Variable(input)
